02ba108f7b368e07927fcd30149558f22fb927c8
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-08-29 20:19:55 +0200

    chore: add husky

---
+++ app.config.js
@@ -0,0 +1,40 @@
+/**
+ * Dynamic app config file of expo app
+ * This configuration file is loaded build time of JS code
+ * More info: https://docs.expo.io/workflow/configuration/#app-config
+ */
+
+//import "dotenv/config";
+
+export default {
+  name: "flow-launcher",
+  slug: "flow-launcher",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    //usesAppleSignIn: true,
+    //bundleIdentifier: "com.koelkast.surfjournalapp",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/images/favicon.png",
+  },
+};
