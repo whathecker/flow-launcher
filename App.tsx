e77626480dd2abfebb9c7f4f23e286efd0cdfd7c
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-10-01 20:15:39 +0200

    chore: add config and packages for Expo dev client

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
 import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import * as React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./src/navigation";
+import Realm from "realm";
+
+
+async function openDatabase() {
+  const Cat = {
+    name: "Cat",
+    properties: {
+      _id: "objectId",
+      name: "string",
+      age: "int",
+      type: "string",
+    },
+  };
+  // open a local realm with the 'Cat' schema
+  const realm = await Realm.open({
+    schema: [Cat],
+  });
+
+  console.log(realm);
+}
 
 export default function App() {
+
+  useEffect(() => {
+    openDatabase();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Navigation />
