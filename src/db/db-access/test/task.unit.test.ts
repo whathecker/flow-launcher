21faf9079a4261348a961d09397c6e8e0971a735
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2021-10-05 20:54:22 +0200

    fix: use dropTasks in unit test of db-access for task object

--- src/db/db-access/test/task.unit.test.ts
+++ src/db/db-access/test/task.unit.test.ts
@@ -2,16 +2,16 @@ import taskDB from "../task";
 
 describe("Test db access module of Task object", () => {
   beforeEach(async () => {
-    await taskDB.dropGoals();
+    await taskDB.dropTasks();
     // add some mock goals here
   });
 
   afterAll(async () => {
-    await taskDB.dropGoals();
+    await taskDB.dropTasks();
   });
 
   test("Delete all tasks", async () => {
-    await taskDB.dropGoals();
+    await taskDB.dropTasks();
     expect(1).toBe(1);
   });
 });
