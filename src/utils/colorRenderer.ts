fe2df8b74f98fb3fee17d63f408d812833be2faa
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-02-20 19:47:11 +0100

    build: get color share per prio bucket

--- src/utils/colorRenderer.ts
+++ src/utils/colorRenderer.ts
@@ -1,4 +1,12 @@
 import { GoalColor } from "../types/core/entity";
+import {
+  PriorityTier,
+  PriorityBucketColor,
+  ColorShadeForFirstGoalColor,
+  ColorShadeForSecondGoalColor,
+  ColorShadeForThirdGoalColor,
+  ColorShadeForFourthGoalColor,
+} from "../types/core/value-object";
 
 const assignColorForGoal = (index: number): GoalColor => {
   if (_isFirstColor(index)) return "#E9695B";
@@ -32,4 +40,125 @@ function _findColorToRender(index: number): number {
   return index % 4;
 }
 
-export default { assignColorForGoal };
+const getColorForPrioBucket = (
+  goalColor: GoalColor,
+  prio: PriorityTier,
+): PriorityBucketColor | "#FEFEF8" => {
+  let color;
+  switch (goalColor) {
+    case "#E9695B":
+      color = _findColorShadeForFirstColor(prio);
+      break;
+    case "#84C5E8":
+      color = _findColorShadeForSecondColor(prio);
+      break;
+    case "#1C88BA":
+      color = _findColorShadeForThirdColor(prio);
+      break;
+    case "#F0B541":
+      color = _findColorShadeForFourthColor(prio);
+      break;
+    default:
+      color = "#FEFEF8";
+      break;
+  }
+  return color as PriorityBucketColor;
+};
+
+function _findColorShadeForFirstColor(
+  prio: PriorityTier,
+): ColorShadeForFirstGoalColor | "#FEFEF8" {
+  let color;
+  switch (prio) {
+    case "highest":
+      color = "#C2571A";
+      break;
+    case "high":
+      color = "#DA621E";
+      break;
+    case "mid":
+      color = "#F26D21";
+      break;
+    case "low":
+      color = "#F58B4C";
+      break;
+    default:
+      color = "#FEFEF8";
+      break;
+  }
+  return color as ColorShadeForFirstGoalColor;
+}
+
+function _findColorShadeForSecondColor(
+  prio: PriorityTier,
+): ColorShadeForSecondGoalColor | "#FEFEF8" {
+  let color;
+  switch (prio) {
+    case "highest":
+      color = "#107896";
+      break;
+    case "high":
+      color = "#0D9BC3";
+      break;
+    case "mid":
+      color = "#2FAFD3";
+      break;
+    case "low":
+      color = "#65CBE9";
+      break;
+    default:
+      color = "#FEFEF8";
+      break;
+  }
+  return color as ColorShadeForSecondGoalColor;
+}
+
+function _findColorShadeForThirdColor(
+  prio: PriorityTier,
+): ColorShadeForThirdGoalColor | "#FEFEF8" {
+  let color;
+  switch (prio) {
+    case "highest":
+      color = "#093145";
+      break;
+    case "high":
+      color = "#0D3D56";
+      break;
+    case "mid":
+      color = "#0D5275";
+      break;
+    case "low":
+      color = "#3C6478";
+      break;
+    default:
+      color = "#FEFEF8";
+      break;
+  }
+  return color as ColorShadeForThirdGoalColor;
+}
+
+function _findColorShadeForFourthColor(
+  prio: PriorityTier,
+): ColorShadeForFourthGoalColor | "#FEFEF8" {
+  let color;
+  switch (prio) {
+    case "highest":
+      color = "#BCA136";
+      break;
+    case "high":
+      color = "#D3B53D";
+      break;
+    case "mid":
+      color = "#0D5275";
+      break;
+    case "low":
+      color = "#EBC944";
+      break;
+    default:
+      color = "#FEFEF8";
+      break;
+  }
+  return color as ColorShadeForFourthGoalColor;
+}
+
+export default { assignColorForGoal, getColorForPrioBucket };
--- src/utils/tests/colorRenderer.test.ts
+++ src/utils/tests/colorRenderer.test.ts
@@ -23,4 +23,60 @@ describe("Test color renderer module", () => {
     expect(fifthColor).toBe(colorTwo);
     expect(sixthColor).toBe(colorThree);
   });
+
+  it("Should render correct color share per prio bucket - first goal color", () => {
+    const highest = colorRenderer.getColorForPrioBucket("#E9695B", "highest");
+    const high = colorRenderer.getColorForPrioBucket("#E9695B", "high");
+    const mid = colorRenderer.getColorForPrioBucket("#E9695B", "mid");
+    const low = colorRenderer.getColorForPrioBucket("#E9695B", "low");
+    const na = colorRenderer.getColorForPrioBucket("#E9695B", "n/a");
+
+    expect(highest).toBe("#C2571A");
+    expect(high).toBe("#DA621E");
+    expect(mid).toBe("#F26D21");
+    expect(low).toBe("#F58B4C");
+    expect(na).toBe("#FEFEF8");
+  });
+
+  it("Should render correct color share per prio bucket - second goal color", () => {
+    const highest = colorRenderer.getColorForPrioBucket("#84C5E8", "highest");
+    const high = colorRenderer.getColorForPrioBucket("#84C5E8", "high");
+    const mid = colorRenderer.getColorForPrioBucket("#84C5E8", "mid");
+    const low = colorRenderer.getColorForPrioBucket("#84C5E8", "low");
+    const na = colorRenderer.getColorForPrioBucket("#84C5E8", "n/a");
+
+    expect(highest).toBe("#107896");
+    expect(high).toBe("#0D9BC3");
+    expect(mid).toBe("#2FAFD3");
+    expect(low).toBe("#65CBE9");
+    expect(na).toBe("#FEFEF8");
+  });
+
+  it("Should render correct color share per prio bucket - third goal color", () => {
+    const highest = colorRenderer.getColorForPrioBucket("#1C88BA", "highest");
+    const high = colorRenderer.getColorForPrioBucket("#1C88BA", "high");
+    const mid = colorRenderer.getColorForPrioBucket("#1C88BA", "mid");
+    const low = colorRenderer.getColorForPrioBucket("#1C88BA", "low");
+    const na = colorRenderer.getColorForPrioBucket("#1C88BA", "n/a");
+
+    expect(highest).toBe("#093145");
+    expect(high).toBe("#0D3D56");
+    expect(mid).toBe("#0D5275");
+    expect(low).toBe("#3C6478");
+    expect(na).toBe("#FEFEF8");
+  });
+
+  it("Should render correct color share per prio bucket - fourth goal color", () => {
+    const highest = colorRenderer.getColorForPrioBucket("#F0B541", "highest");
+    const high = colorRenderer.getColorForPrioBucket("#F0B541", "high");
+    const mid = colorRenderer.getColorForPrioBucket("#F0B541", "mid");
+    const low = colorRenderer.getColorForPrioBucket("#F0B541", "low");
+    const na = colorRenderer.getColorForPrioBucket("#F0B541", "n/a");
+
+    expect(highest).toBe("#BCA136");
+    expect(high).toBe("#D3B53D");
+    expect(mid).toBe("#0D5275");
+    expect(low).toBe("#EBC944");
+    expect(na).toBe("#FEFEF8");
+  });
 });
