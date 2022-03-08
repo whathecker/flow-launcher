4290466b69aa20a0b6bd3f6f05b704b41220ce87
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-03-08 19:37:29 +0100

    build: empty prio bucket not clickable

--- src/components/GoalDetail/components/TaskBucketByPrio.tsx
+++ src/components/GoalDetail/components/TaskBucketByPrio.tsx
@@ -6,27 +6,19 @@ import { Task, GoalColor } from "../../../types/core/entity";
 import { PriorityTier } from "../../../types/core/value-object";
 import { colorRenderer, labelRenderer } from "../../../utils";
 
-type TaskBucketByPrioProps = {
+type ActiveBucketProps = {
   prio: PriorityTier;
+  backgroundColor: string;
+  textColor: string;
   tasks: Task[];
-  goalColor: GoalColor;
 };
 
-const TaskBucketByPrio: React.FC<TaskBucketByPrioProps> = ({
+const ActiveBucket: React.FC<ActiveBucketProps> = ({
   prio,
   tasks,
-  goalColor,
-}: TaskBucketByPrioProps) => {
-  const isTasksEmpty = tasks.length === 0;
-
-  const backgroundColor = isTasksEmpty
-    ? "#FEFEF8"
-    : colorRenderer.getColorForPrioBucket(goalColor, prio);
-
-  const textColor = isTasksEmpty
-    ? Color.light.subtleLabel
-    : Color.light.whiteText;
-
+  backgroundColor,
+  textColor,
+}: ActiveBucketProps) => {
   return (
     <Touchable
       style={{
@@ -59,65 +51,152 @@ const TaskBucketByPrio: React.FC<TaskBucketByPrioProps> = ({
           {labelRenderer.renderPrioBucketLabel(prio)}
         </Text>
       </View>
-      {isTasksEmpty ? (
-        <View
+
+      <View
+        style={{
+          backgroundColor: backgroundColor,
+          paddingTop: "3%",
+          paddingBottom: "2%",
+          paddingLeft: "10%",
+        }}
+      >
+        <Text
           style={{
-            ...Container.centerAligned,
-            backgroundColor: backgroundColor,
-            paddingTop: "8%",
+            ...Typography.p,
+            color: textColor,
+            fontSize: 16,
           }}
-        >
-          <Text
-            style={{
-              ...Typography.p,
-              fontSize: 16,
-              color: textColor,
-            }}
-          >{`No awaiting tasks`}</Text>
-        </View>
-      ) : (
-        <View
-          style={{
-            backgroundColor: backgroundColor,
-            paddingTop: "3%",
-            paddingBottom: "2%",
-            paddingLeft: "10%",
-          }}
-        >
+        >{`${tasks[0].title}`}</Text>
+        {tasks.length > 1 ? (
           <Text
             style={{
               ...Typography.p,
               color: textColor,
-              fontSize: 16,
+              fontSize: 15,
+              textDecorationLine: "underline",
+              paddingTop: 5,
             }}
-          >{`${tasks[0].title}`}</Text>
-          {tasks.length > 1 ? (
-            <Text
-              style={{
-                ...Typography.p,
-                color: textColor,
-                fontSize: 15,
-                textDecorationLine: "underline",
-                paddingTop: 5,
-              }}
-            >{`+ ${tasks.length - 1} more`}</Text>
-          ) : null}
-          <View
-            style={{
-              backgroundColor: backgroundColor,
-              position: "absolute",
-              top: -18,
-              right: 15,
-            }}
-          >
-            <Image
-              style={{ width: 18, height: 18 }}
-              source={require(`../../../../assets/images/right-arrow-white.png`)}
-            />
-          </View>
+          >{`+ ${tasks.length - 1} more`}</Text>
+        ) : null}
+        <View
+          style={{
+            backgroundColor: backgroundColor,
+            position: "absolute",
+            top: -18,
+            right: 15,
+          }}
+        >
+          <Image
+            style={{ width: 18, height: 18 }}
+            source={require(`../../../../assets/images/right-arrow-white.png`)}
+          />
         </View>
-      )}
+      </View>
     </Touchable>
+  );
+};
+
+type EmptyBucketProps = {
+  prio: PriorityTier;
+  backgroundColor: string;
+  textColor: string;
+  tasks?: Task[];
+};
+
+const EmptyBucket: React.FC<EmptyBucketProps> = ({
+  prio,
+  backgroundColor,
+  textColor,
+}: EmptyBucketProps) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 120,
+        backgroundColor: backgroundColor,
+        marginTop: 5,
+        marginBottom: "3%",
+        paddingTop: "4%",
+        paddingBottom: "8%",
+        borderWidth: 1,
+        borderColor: Color.light.defaultBorder,
+        borderRadius: 5,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: backgroundColor,
+          paddingLeft: 15,
+        }}
+      >
+        <Text
+          style={{
+            ...Typography.h4,
+            fontSize: 18,
+            fontWeight: "bold",
+            color: textColor,
+          }}
+        >
+          {labelRenderer.renderPrioBucketLabel(prio)}
+        </Text>
+      </View>
+      <View
+        style={{
+          ...Container.centerAligned,
+          backgroundColor: backgroundColor,
+          paddingTop: "8%",
+        }}
+      >
+        <Text
+          style={{
+            ...Typography.p,
+            fontSize: 16,
+            color: textColor,
+          }}
+        >{`No awaiting tasks`}</Text>
+      </View>
+    </View>
+  );
+};
+
+type TaskBucketByPrioProps = {
+  prio: PriorityTier;
+  tasks: Task[];
+  goalColor: GoalColor;
+};
+
+const TaskBucketByPrio: React.FC<TaskBucketByPrioProps> = ({
+  prio,
+  tasks,
+  goalColor,
+}: TaskBucketByPrioProps) => {
+  const isTasksEmpty = tasks.length === 0;
+
+  const backgroundColor = isTasksEmpty
+    ? "#FEFEF8"
+    : colorRenderer.getColorForPrioBucket(goalColor, prio);
+
+  const textColor = isTasksEmpty
+    ? Color.light.subtleLabel
+    : Color.light.whiteText;
+
+  return (
+    <>
+      {isTasksEmpty ? (
+        <EmptyBucket
+          prio={prio}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+        />
+      ) : (
+        <ActiveBucket
+          prio={prio}
+          tasks={tasks}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+        />
+      )}
+    </>
   );
 };
 
